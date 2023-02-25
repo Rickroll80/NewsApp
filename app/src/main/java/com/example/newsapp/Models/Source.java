71fe4bb79d33e90cff0748b331bb01475f3e144c
Rickroll80 <belooseskyj12@gmail.com>
2023-02-25 18:45:27 -0500

    Initial commit with .xml file and Source class to handle "source" JSON response object.

---
+++ app/src/main/java/com/example/newsapp/Models/Source.java
@@ -0,0 +1,15 @@
+package com.example.newsapp.Models;
+
+public class Source {
+    /*
+    "source": {
+                "id": "google-news",
+                "name": "Google News"
+            },
+     */
+    String id = "";
+    String name = "";
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+}
